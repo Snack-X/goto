5971bdfce75662849e5ac27984994716cd363aef
Snack <korsnack@korsnack.kr>
2015-07-03 00:45:51 +0900

    Use Noto Sans

--- view/templates/header.php
+++ view/templates/header.php
@@ -5,6 +5,7 @@
 <meta name="viewport" content="width=device-width, user-scalable=no">
 <title><?php echo $title; ?></title>
 <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Maven+Pro:400,700">
+<link rel="stylesheet" href="http://data.korsnack.kr/static/fonts/NotoSansKR.css">
 <link rel="stylesheet" href="/assets/css/normalize.min.css">
 <link rel="stylesheet" href="/assets/css/geomicons.min.css">
 <link rel="stylesheet" href="/assets/css/page_global.min.css">
